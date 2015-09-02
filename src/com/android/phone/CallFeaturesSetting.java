0af3e5c55d525f6c94212dc0a5aab5f321ee72dd
ljjehl <ljjehl@gmail.com>
2015-09-02 23:33:45 +0200

    Revert "[2/3] Telephony: Proximity speaker"
    This reverts commit 3ca8379af6405b9cb9db9a9b420a0dd9f59d0ed9.
    
    Conflicts:
    	src/com/android/phone/CallFeaturesSetting.java
    
    Revert Prox Speaker: Cleanup
    
    Change-Id: Iaed05faf923dc2471d933fbd0a786d357d7e4596

--- src/com/android/phone/CallFeaturesSetting.java
+++ src/com/android/phone/CallFeaturesSetting.java
@@ -21,7 +21,6 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.ProgressDialog;
-import android.content.ContentResolver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -40,7 +39,6 @@ import android.os.AsyncResult;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.os.PowerManager;
 import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.preference.CheckBoxPreference;
@@ -50,7 +48,6 @@ import android.preference.PreferenceActivity;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceManager;
 import android.preference.PreferenceScreen;
-import android.preference.SlimSeekBarPreference;
 import android.preference.SwitchPreference;
 import android.provider.ContactsContract.CommonDataKinds;
 import android.provider.Settings;
@@ -194,10 +191,6 @@ public class CallFeaturesSetting extends PreferenceActivity
     private static final String BUTTON_VIDEO_CALL_FW_KEY = "videocall_setting_fw_key";
     private static final String BUTTON_VIDEO_CALL_SP_KEY = "vt_imageplacer";
     private static final String BUTTON_VIDEO_CALL_SWITCH = "button_enable_video_calling";
-
-    private static final String PROX_AUTO_SPEAKER  = "prox_auto_speaker";
-    private static final String PROX_AUTO_SPEAKER_DELAY  = "prox_auto_speaker_delay";
-    private static final String PROX_AUTO_SPEAKER_INCALL_ONLY  = "prox_auto_speaker_incall_only";
 
     private static final String BUTTON_GSM_UMTS_OPTIONS = "button_gsm_more_expand_key";
     private static final String BUTTON_CDMA_OPTIONS = "button_cdma_more_expand_key";
@@ -398,10 +391,6 @@ public class CallFeaturesSetting extends PreferenceActivity
 
     private SharedPreferences mPerProviderSavedVMNumbers;
 
-    private SwitchPreference mProxSpeaker;
-    private SlimSeekBarPreference mProxSpeakerDelay;
-    private SwitchPreference mProxSpeakerIncallOnly;
-
     /**
      * Results of reading forwarding settings
      */
@@ -519,13 +508,6 @@ public class CallFeaturesSetting extends PreferenceActivity
             Settings.System.putInt(getContentResolver(), Settings.System.ENABLE_MWI_NOTIFICATION,
                     mMwiNotification.isChecked() ? 1 : 0);
             return true;
-        } else if (preference == mProxSpeaker) {
-            Settings.System.putInt(getContentResolver(), Settings.System.PROXIMITY_AUTO_SPEAKER,
-                    mProxSpeaker.isChecked() ? 1 : 0);
-        } else if (preference == mProxSpeakerIncallOnly) {
-            Settings.System.putInt(getContentResolver(),
-                    Settings.System.PROXIMITY_AUTO_SPEAKER_INCALL_ONLY,
-                    mProxSpeakerIncallOnly.isChecked() ? 1 : 0);
         } else if (preference == mButtonDTMF) {
             return true;
         } else if (preference == mButtonTTY) {
@@ -636,7 +618,6 @@ public class CallFeaturesSetting extends PreferenceActivity
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
 
-        ContentResolver cr = mPhone.getContext().getContentResolver();
 
         if (DBG) {
             log("onPreferenceChange(). preferenece: \"" + preference + "\""
@@ -645,7 +626,8 @@ public class CallFeaturesSetting extends PreferenceActivity
 
         if (preference == mButtonDTMF) {
             int index = mButtonDTMF.findIndexOfValue((String) objValue);
-            Settings.System.putInt(cr, Settings.System.DTMF_TONE_TYPE_WHEN_DIALING, index);
+Settings.System.putInt(mPhone.getContext().getContentResolver(),
+                    Settings.System.DTMF_TONE_TYPE_WHEN_DIALING, index);        
         } else if (preference == mButtonTTY) {
             handleTTYChange(preference, objValue);
         } else if (preference == mVoicemailProviders) {
@@ -681,15 +663,6 @@ public class CallFeaturesSetting extends PreferenceActivity
                 mChangingVMorFwdDueToProviderChange = true;
                 saveVoiceMailAndForwardingNumber(newProviderKey, newProviderSettings);
             }
-        } else if (preference == mProxSpeakerDelay) {
-            int delay = Integer.valueOf((String) objValue);
-            Settings.System.putInt(getContentResolver(),
-                    Settings.System.PROXIMITY_AUTO_SPEAKER_DELAY, delay);
-        } else if (preference == mCallRecordingFormat) {
-            int value = Integer.valueOf((String) objValue);
-            int index = mCallRecordingFormat.findIndexOfValue((String) objValue);
-            Settings.System.putInt(cr, Settings.System.CALL_RECORDING_FORMAT, value);
-            mCallRecordingFormat.setSummary(mCallRecordingFormat.getEntries()[index]);
         }
         // always let the preference setting proceed.
         return true;
@@ -1758,17 +1731,6 @@ public class CallFeaturesSetting extends PreferenceActivity
             initVoiceMailProviders();
         }
 
-        mProxSpeaker = (SwitchPreference) findPreference(PROX_AUTO_SPEAKER);
-        mProxSpeakerIncallOnly = (SwitchPreference) findPreference(PROX_AUTO_SPEAKER_INCALL_ONLY);
-        mProxSpeakerDelay = (SlimSeekBarPreference) findPreference(PROX_AUTO_SPEAKER_DELAY);
-        if (mProxSpeakerDelay != null) {
-            mProxSpeakerDelay.setDefault(100);
-            mProxSpeakerDelay.isMilliseconds(true);
-            mProxSpeakerDelay.setInterval(1);
-            mProxSpeakerDelay.minimumValue(100);
-            mProxSpeakerDelay.multiplyValue(100);
-            mProxSpeakerDelay.setOnPreferenceChangeListener(this);
-        }
 
         if (mButtonDTMF != null) {
             if (getResources().getBoolean(R.bool.dtmf_type_enabled)) {
@@ -1817,39 +1779,6 @@ public class CallFeaturesSetting extends PreferenceActivity
             } else {
                 prefSet.removePreference(mButtonTTY);
                 mButtonTTY = null;
-            }
-        }
-
-        final ContentResolver contentResolver = getContentResolver();
-
-        if (mProxSpeaker != null) {
-            PowerManager pm = (PowerManager) this.getSystemService(Context.POWER_SERVICE);
-            if (pm.isWakeLockLevelSupported(
-                    PowerManager.PROXIMITY_SCREEN_OFF_WAKE_LOCK)
-                    && getResources().getBoolean(R.bool.config_enabled_speakerprox)) {
-                mProxSpeaker.setChecked(Settings.System.getInt(contentResolver,
-                        Settings.System.PROXIMITY_AUTO_SPEAKER, 0) == 1);
-                if (mProxSpeakerIncallOnly != null) {
-                    mProxSpeakerIncallOnly.setChecked(Settings.System.getInt(contentResolver,
-                            Settings.System.PROXIMITY_AUTO_SPEAKER_INCALL_ONLY, 0) == 1);
-                }
-                if (mProxSpeakerDelay != null) {
-                    final int proxDelay = Settings.System.getInt(getContentResolver(),
-                            Settings.System.PROXIMITY_AUTO_SPEAKER_DELAY, 100);
-                    // minimum 100 is 1 interval of the 100 multiplier
-                    mProxSpeakerDelay.setInitValue((proxDelay / 100) - 1);
-                }
-            } else {
-                prefSet.removePreference(mProxSpeaker);
-                mProxSpeaker = null;
-                if (mProxSpeakerIncallOnly != null) {
-                    prefSet.removePreference(mProxSpeakerIncallOnly);
-                    mProxSpeakerIncallOnly = null;
-                }
-                if (mProxSpeakerDelay != null) {
-                    prefSet.removePreference(mProxSpeakerDelay);
-                    mProxSpeakerDelay = null;
-                }
             }
         }
 
