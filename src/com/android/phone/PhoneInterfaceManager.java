bd9a841ed5fc8dafbf305406369c137070f0f02c
dadi11 <dadi.sikorski@gmail.com>
2017-03-15 17:28:50 +0100

    Revert "Implement toggleLte"
    Revert "toggleLTE: switch to Global mode on devices that default to it"
    
    This reverts commit dab7cb2c13cdc90dcaf4f59582a6ad1c5f968cfa.
    
    Revert "Restore previous network value after toggleLTE"
    
    This reverts commit ae4f22ee5a6acf3cda2a49c068f38cdd4efdaa5f.
    
    Revert "Implement toggleLte"
    
    This reverts commit de40bfec575a05a0d5598766f6851be5b90bb38d.

--- src/com/android/phone/PhoneInterfaceManager.java
+++ src/com/android/phone/PhoneInterfaceManager.java
@@ -87,10 +87,6 @@ import com.android.phone.settings.VisualVoicemailSettingsUtil;
 import com.android.phone.settings.VoicemailNotificationSettingsUtil;
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
-
-import static com.android.internal.telephony.PhoneConstants.SUBSCRIPTION_KEY;
-import com.android.internal.telephony.RILConstants;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -156,7 +152,6 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
     private static final int EVENT_SIM_GET_ATR_DONE = 48;
     private static final int CMD_OPEN_CHANNEL_WITH_P2 = 49;
     private static final int CMD_TOGGLE_2G = 98;
-    private static final int CMD_TOGGLE_LTE = 99; // not used yet
 
     private static final String PRIMARY_CARD_PROPERTY_NAME = "persist.radio.primarycard";
 
@@ -1121,17 +1116,6 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
         mApp.startActivity(intent);
     }
 
-    public int getPreferredNetworkMode() {
-        int preferredNetworkMode = RILConstants.PREFERRED_NETWORK_MODE;
-        if (mPhone.getLteOnCdmaMode() == PhoneConstants.LTE_ON_CDMA_TRUE) {
-            preferredNetworkMode = Phone.NT_MODE_GLOBAL;
-        }
-        final int phoneSubId = mPhone.getSubId();
-        int network = Settings.Global.getInt(mPhone.getContext().getContentResolver(),
-              Settings.Global.PREFERRED_NETWORK_MODE + phoneSubId, preferredNetworkMode);
-        return network;
-    }
-
     public void toggle2G(boolean on) {
         int network = -1;
         final int phoneSubId = mSubscriptionController.getDefaultDataSubId();
@@ -1162,59 +1146,6 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
 
         log("DefaultSubId: " + phoneSubId);
         log("NetworkType: " + network);
-    }
-
-    public void toggleLTE(boolean on) {
-        int network = getPreferredNetworkMode();
-        boolean isCdmaDevice = mPhone.getLteOnCdmaMode() == PhoneConstants.LTE_ON_CDMA_TRUE;
-
-        switch (network) {
-        // GSM Devices
-        case Phone.NT_MODE_WCDMA_PREF:
-        case Phone.NT_MODE_GSM_UMTS:
-        case Phone.NT_MODE_GSM_ONLY:
-            //push old network to useless Settings.Global.PREFERRED_NETWORK_MODE
-            android.provider.Settings.Global.putInt(mApp.getContentResolver(),
-                    android.provider.Settings.Global.PREFERRED_NETWORK_MODE, network);
-            network = Phone.NT_MODE_LTE_GSM_WCDMA;
-            break;
-        case Phone.NT_MODE_LTE_GSM_WCDMA:
-            network = Settings.Global.getInt(mPhone.getContext().getContentResolver(),
-              Settings.Global.PREFERRED_NETWORK_MODE, 1);
-            break;
-        // GSM and CDMA devices
-        case Phone.NT_MODE_GLOBAL:
-            // Wtf to do here?
-            network = Phone.NT_MODE_LTE_CDMA_EVDO_GSM_WCDMA;
-            break;
-        case Phone.NT_MODE_LTE_CDMA_EVDO_GSM_WCDMA:
-            // Determine the correct network type
-            if (isCdmaDevice) {
-                network = Phone.NT_MODE_CDMA;
-            } else {
-                network = Phone.NT_MODE_WCDMA_PREF;
-            }
-            break;
-        // CDMA Devices
-        case Phone.NT_MODE_CDMA:
-            if (SystemProperties.getInt("ro.telephony.default_network", 0) ==
-                        RILConstants.NETWORK_MODE_LTE_CDMA_EVDO_GSM_WCDMA) {
-                network = Phone.NT_MODE_LTE_CDMA_EVDO_GSM_WCDMA;
-            } else {
-                network = Phone.NT_MODE_LTE_CDMA_AND_EVDO;
-            }
-            break;
-        case Phone.NT_MODE_LTE_CDMA_AND_EVDO:
-            network = Phone.NT_MODE_CDMA;
-            break;
-        }
-
-        mPhone.setPreferredNetworkType(network,
-                mMainThreadHandler.obtainMessage(CMD_TOGGLE_LTE));
-        final int phoneSubId = mPhone.getSubId();
-        android.provider.Settings.Global.putInt(mApp.getContentResolver(),
-                android.provider.Settings.Global.PREFERRED_NETWORK_MODE + phoneSubId, network);
-
     }
 
     /**
